675aa0fbf88f1bb6aac801d38c09068bcdd19647
Josep Lluís Berral García <josepllberral@gmail.com>
2014-11-28 15:17:48 +0100

    Added support for loading reduced dimensionality learned models through CLI

--- aloja_cli.r
+++ aloja_cli.r
@@ -5,13 +5,17 @@
 # 2014-11-24
 # Launcher of ALOJA-ML
  
-# usage: ./aloja_cli.r -d dataset.csv -m aloja_regtree -p param1=aaaa:param2=bbbb:param3=cccc:...
-#	 ./aloja_cli.r --dataset dataset.csv --method aloja_regtree --params param1=aaaa:param2=bbbb:param3=cccc:...
-#	 ./aloja_cli.r -d dataset.csv -m aloja_pca -p saveall="pca1"
-#	 ./aloja_cli.r -d pca1-dataset.csv -m aloja_regtree -p prange=1e-4,1e+4 -a
-#	 ./aloja_cli.r -d pca1-transformed.csv -m aloja_regtree -p prange=1e-4,1e+4 -n 20
-#	 ./aloja_cli.r -m aloja_predict_instance -l m5p-model -p inst_predict="sort,ETH,RR3,8,10,1,65536,None,32,Azure L" -v
-#	 ./aloja_cli.r -m aloja_predict_instance -l m5p-model -d dataset-to-predict.csv -v
+# usage: ./aloja_cli.r -m method [-d dataset] [-l learned model] [-p param1=aaaa:param2=bbbb:param3=cccc:...] [-a] [-n dims] [-v]
+#	 ./aloja_cli.r --method method [--dataset dataset] [--learned learned model] [--params param1=aaaa:param2=bbbb:param3=cccc:...] [--allvars] [--numvars dims] [--verbose]
+#
+#	 ./aloja_cli.r -m aloja_regtree -d aloja-dataset.csv -p saveall=m5p1
+#	 ./aloja_cli.r -m aloja_predict_instance -l m5p1 -p inst_predict="sort,ETH,RR3,8,10,1,65536,None,32,Azure L" -v
+#	 ./aloja_cli.r -m aloja_predict_dataset -l m5p1 -d m5p1-tt.csv -v
+#
+#	 ./aloja_cli.r -m aloja_pca -d dataset.csv -p saveall=pca1
+#	 ./aloja_cli.r -m aloja_regtree -d pca1-transformed.csv -p prange=1e-4,1e+4,saveall=m5p-simple-redim -n 20
+#	 ./aloja_cli.r -m aloja_predict_instance -l m5p-simple-redim -p inst_predict="1922.904354752,70.1570440421649,2.9694955079494,-3.64259027685954,-0.748746678239734,0.161321484374316,0.617610510007444,-0.459044093400257,0.251211132013151,0.251937462205716,-0.142007748147355,-0.0324862729758309,0.406308900544488,0.13593705166432,0.397452596451088,-0.731635384355167,-0.318297127484775,-0.0876192175148721,-0.0504762335523307,-0.0146283091875174" -v
+#	 ./aloja_cli.r -m aloja_predict_dataset -l m5p-simple-redim -d m5p-simple-redim-tt.csv -v
 
 source("functions.r");
 
@@ -61,26 +65,25 @@ source("functions.r");
 	params <- list();
 	params[["ds"]] <- dataset;
 
-	if (opt$method %in% c("aloja_regtree","aloja_nneighbors","aloja_linreg","aloja_nnet","aloja_pca","aloja_dataset_collapse","aloja_predict_instance","aloja_predict_dataset"))
+	if (opt$method %in% c("aloja_regtree","aloja_nneighbors","aloja_linreg","aloja_nnet","aloja_pca","aloja_dataset_collapse"))
 	{
+		params[["vout"]] = "Exe.Time";
 		if (opt$allvars)
 		{
-			params[["vin"]] = colnames(dataset)[!(colnames(dataset) %in% c("ID","Exe.Time"))];
+			params[["vin"]] = colnames(dataset)[!(colnames(dataset) %in% c("ID",params$vout))];
 		} else if (!is.null(opt$numvars)) {
-			params[["vin"]] = (colnames(dataset)[!(colnames(dataset) %in% c("ID","Exe.Time"))])[1:opt$numvars];
+			params[["vin"]] = (colnames(dataset)[!(colnames(dataset) %in% c("ID",params$vout))])[1:opt$numvars];
 		} else {
 			params[["vin"]] = c("Benchmark","Net","Disk","Maps","IO.SFac","Rep","IO.FBuf","Comp","Blk.size","Cluster");
 		}
-		params[["vout"]] = "Exe.Time";
 	}
 
 	if (opt$method  == "aloja_predict_instance" || opt$method  == "aloja_predict_dataset")
 	{
-		params[["vout"]] <- NULL;
-
 		params_2 <- list();
 		params_2[["tagname"]] <- opt$learned;
 		params[["learned_model"]] <- do.call(aloja_load_object,params_2);
+		params[["vin"]] <- params$learned_model$varin;
 	}
 
 	if (opt$method == "aloja_dataset_clustering")
